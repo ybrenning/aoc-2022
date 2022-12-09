8776dd5e4200648de48fd9d18918e0605d5022bb
ybrenning <yannickbrenning2@gmail.com>
2022-12-09 16:23:18 +0100

    Add day 6

---
+++ 06/06.py
@@ -0,0 +1,27 @@
+def part_one() -> None:
+    with open("input.txt") as f:
+        line = f.readline()
+    
+    for i in range(0, len(line) - 4):
+        if len(set(line[i:i+4])) == len(line[i:i+4]):
+            print(i+4)
+            break
+
+
+def part_two() -> None:
+    with open("input.txt") as f:
+        line = f.readline()
+    
+    for i in range(0, len(line) - 14):
+        if len(set(line[i:i+14])) == len(line[i:i+14]):
+            print(i+14)
+            break
+
+
+def main() -> None:
+    part_one()
+    part_two()
+
+
+if __name__ == "__main__":
+    main()
