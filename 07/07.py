46cd4fe3b648c365fa2a47dfc1320da31ccb314b
ybrenning <yannickbrenning2@gmail.com>
2022-12-12 23:27:22 +0100

    Add day 7 pt 2

--- 07/07.py
+++ 07/07.py
@@ -25,8 +25,42 @@ def part_one() -> None:
     print(sum([size for size in sizes.values() if size <= 100_000]))
 
 
+def part_two() -> None:
+    with open("input.txt") as f:
+        lines = f.readlines()
+
+    sizes: dict[str, int] = defaultdict(int)
+    filepath: list[str] = []
+
+    for line in lines:
+        if(line.startswith("$ cd")):
+            dir = line.split()[-1]
+            if(dir == ".."):
+                filepath.pop()
+            else:
+                filepath.append(dir)
+        elif(line.startswith("$ ls")):
+            continue
+        else:
+            if(line.split()[0].isdigit()):
+                size = int(line.split()[0])
+                for i in range(0, len(filepath)):
+                    sizes["/".join(filepath[:i+1])] += size
+        
+    max_space = 70_000_000 - 30_000_000
+    need_to_free = sizes["/"] - max_space
+    
+    ans = 1e9
+    for size in sizes.values():
+        if size >= need_to_free:
+            ans = min(ans, size)
+    
+    print(ans)
+
+
 def main() -> None:
     part_one()
+    part_two()
 
 
 if __name__ == "__main__":
