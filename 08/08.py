0f62fdbb6f50aa912e974526c44fdd4eccaf8480
ybrenning <yannickbrenning2@gmail.com>
2022-12-12 16:12:29 +0100

    Add day 8

--- 08/08.py
+++ 08/08.py
@@ -22,8 +22,54 @@ def part_one() -> None:
     print(count)
 
 
+def part_two() -> None:
+    with open("input.txt") as f:
+        lines = [line.strip() for line in f.readlines()]
+    
+    max = current_score = upview = downview = lview = rview = 0
+    for i in range(0, len(lines)):
+        for j in range(0, len(lines[i])):
+            left = lines[i][:j]
+            right = lines[i][j+1:]
+            
+            above = [num[j] for num in lines[:i]]
+            below = [num[j] for num in lines[i+1:]]
+            
+            # View trees from correct perspective
+            left, above = left[::-1], above[::-1]
+            
+            for tree in above:
+                upview += 1
+                if int(tree) >= int(lines[i][j]):
+                    break
+            
+            for tree in below:
+                downview += 1
+                if int(tree) >= int(lines[i][j]):
+                    break
+            
+            for tree in left:
+                lview += 1
+                if int(tree) >= int(lines[i][j]):
+                    break
+            
+            for tree in right:
+                rview += 1
+                if int(tree) >= int(lines[i][j]):
+                    break
+            
+            current_score = upview * downview * lview * rview
+            max = current_score if current_score > max else max
+            
+            # Reset view counts
+            upview = downview = lview = rview = 0
+        
+    print(max)
+
+
 def main() -> None:
     part_one()
+    part_two()
 
 
 if __name__ == "__main__":
