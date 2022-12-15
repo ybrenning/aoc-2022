1242a51fab45fd405a218c552f86a8d444342e36
ybrenning <yannickbrenning2@gmail.com>
2022-12-15 15:46:35 +0100

    Add day 12 pt 2

--- 12/12.py
+++ 12/12.py
@@ -99,8 +99,60 @@ def part_one() -> None:
     print(bfs(nodes, start, end))
 
 
+def part_two() -> None:
+    with open("input.txt") as f:
+        lines = [
+            line.strip().replace("\n", "")
+            for line in f.readlines()
+            if line.strip().replace("\n", "")
+        ]
+
+    end = None
+    nodes = []
+    for y in range(0, len(lines)):
+        nodes.append([])
+        for x in range(0, len(lines[y])):
+            elevation = lines[y][x].strip()
+            if elevation == "E":
+                end = Node(x, y, "z")
+                nodes[y].append(end)
+            else:
+                if elevation == "S":
+                    elevation = "a"
+
+                nodes[y].append(Node(x, y, elevation))
+
+    # Get node paths
+    for y in range(0, len(lines)):
+        for x in range(0, len(lines[y])):
+            possible_nexts = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+            possible_nexts = [
+                coord
+                for coord in possible_nexts
+                if 0 <= coord[0] < len(lines[y]) and 0 <= coord[1] < len(lines)
+            ]
+
+            for coord in possible_nexts:
+                # Get the reference to the possible next node
+                xn, yn = coord
+                possible_next = nodes[yn][xn]
+
+                # Check whether it is accessible
+                if ord(possible_next.elevation) - 1 <= ord(nodes[y][x].elevation):
+                    nodes[y][x].next.append(possible_next)
+
+    dists = []
+    for y in range(0, len(nodes)):
+        for x in range(0, len(nodes[y])):
+            if nodes[y][x].elevation == "a":
+                dists.append(bfs(nodes, nodes[y][x], end))
+
+    print(min(filter(lambda x: x >= 0, dists)))
+
+
 def main() -> None:
     part_one()
+    part_two()
 
 
 if __name__ == "__main__":
